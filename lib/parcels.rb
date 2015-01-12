a8c209d1df011329ef8ee479b3b23025107dc1c3
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-12 14:34:20 -0800

    Add full functionality

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -11,21 +11,25 @@ class Parcel
   end
 
   define_method(:cost_to_ship) do
-    if self.volume().*(@weight).!=(0)
-      if self.volume().<=(100)
-        "Our low flat-rate of $9.95"
+    if self.volume().*(@weight).>(0)
+      if @weight.>(80)
+        if self.volume().>(30000)
+          "Your package is too heavy AND too large to ship"
+        else
+          "Your package is too heavy to ship"
+        end
       elsif self.volume().>(30000)
         "Your package is too large to ship"
-      elsif @weight.>(80)
-        "Your package is too heavy to ship"
+      elsif self.volume().<(100) && @weight.<(1)
+        "Our low flat-rate of $9.95"
       else
-        vol_mass = self.volume().*(0.005)
+        vol_mass = self.volume().*(0.001)
         if vol_mass.>=(@weight)
-          total_cost = vol_mass.*(20)
+          total_cost = vol_mass.*(10)
         else
-          total_cost = @weight*(20)
+          total_cost = @weight*(10)
         end
-        "Your total cost to ship this package is ".+(total_cost.to_s)
+        "Your total cost to ship this package is $".+(total_cost.round(2).to_s())
       end
     else
       "Your package does not exist in this plane of being"
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -26,6 +26,35 @@ describe(Parcel) do
       expect(test_parcel.cost_to_ship()).to(eq("Your package does not exist in this plane of being"))
     end
 
+    it("returns the flat rate when volume is under 100 sq inches and less than half a pound") do
+      test_parcel = Parcel.new(2,2,24,0.4)
+      expect(test_parcel.cost_to_ship()).to(eq("Our low flat-rate of $9.95"))
+    end
+
+    it("returns an error message when the package is too unwieldy for our folks to handle") do
+      test_parcel = Parcel.new(400,400,400,1)
+      expect(test_parcel.cost_to_ship()).to(eq("Your package is too large to ship"))
+    end
+
+    it("returns an error message when the package is too heavy for our folks to handle") do
+      test_parcel = Parcel.new(1,1,1,1000)
+      expect(test_parcel.cost_to_ship()).to(eq("Your package is too heavy to ship"))
+    end
+
+    it("returns an error message when the package is too heavy AND too unwieldy for our folks to handle") do
+      test_parcel = Parcel.new(400,400,400,1000)
+      expect(test_parcel.cost_to_ship()).to(eq("Your package is too heavy AND too large to ship"))
+    end
+
+    it("returns the calculated price based on the inputted weight") do
+      test_parcel = Parcel.new(10,10,10,10)
+      expect(test_parcel.cost_to_ship()).to(eq("Your total cost to ship this package is $100.0"))
+    end
+
+    it("returns the calculated price based on the volume-mass for light but large packages") do
+      test_parcel = Parcel.new(20,20,20,1)
+      expect(test_parcel.cost_to_ship()).to(eq("Your total cost to ship this package is $80.0"))
+    end
 
   end
 
